915d2e14ec12161c343f3d2aedac29ba8e1ef0fa
LeHec710 <hector.bidan@gmail.com>
2022-06-06 15:54:37 +0200

    cleaning stuff

--- assets/js/scrollbar.js
+++ assets/js/scrollbar.js
@@ -1,38 +1,48 @@
 // making the scrollbar effect
-scrollbar = document.querySelector('.scrollbar');
-arrow = document.querySelector('.scrollbar #scroll-arrow');
-topline = document.querySelector('.scrollbar .line#top');
-bottomline = document.querySelector('.scrollbar .line#bottom');
+scrollbar = document.querySelector(".scrollbar");
+arrow = document.querySelector(".scrollbar #scroll-arrow");
+topline = document.querySelector(".scrollbar .line#top");
+bottomline = document.querySelector(".scrollbar .line#bottom");
+scrollup = document.querySelector(".scroll");
 
-let totalHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+// scroll up button
+scrollup.addEventListener("click", () => {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+});
 
 // scroll animation
-window.addEventListener('scroll', ()  => {
-    let progress = (document.documentElement.scrollTop / document.documentElement.clientHeight) * 100;
-    console.log(progress);
-    arrow.style.top = `${scrollToX(progress)}`
-    scrollToX(progress)
-})
+window.addEventListener("scroll", () => {
+  let progress =
+    (document.documentElement.scrollTop /
+      document.documentElement.clientHeight) *
+    100;
+  console.log(progress);
+  arrow.style.top = `${scrollToX(progress)}`;
+  scrollToX(progress);
+});
 
 // click to scroll
-scrollbar.addEventListener('click', (e) => {
-    let newScroll = e.clientY / scrollbar.offsetHeight * totalHeight;
-    window.scrollTo({
-        top: newScroll,
-        behavior: 'smooth'
-    })
-})
+scrollbar.addEventListener("click", (e) => {
+  let newScroll =
+    (e.clientY / scrollbar.offsetHeight) *
+    document.documentElement.clientHeight;
+  window.scrollTo({
+    top: newScroll,
+    behavior: "smooth",
+  });
+  scrollToX(newScroll);
+});
 
 // function to scroll to position
 function scrollToX(x) {
-    // equal to the side gap in % in main.scss
-    x =  Math.min(Math.max(x, 0), 100);
-    // positionning the arrow
-    arrow.style.top = x + "%";
-    topline.style.height = "calc(" + x + "% + -2rem)";
-    bottomline.style.height = "calc(100% - " + x + "% - 2rem)";
+  // equal to the side gap in % in main.scss
+  x = Math.min(Math.max(x, 0), 100);
+  // positionning the arrow
+  arrow.style.top = x + "%";
+  topline.style.height = "calc(" + x + "% + -2rem)";
+  bottomline.style.height = "calc(100% - " + x + "% - 2rem)";
 }
 
 function isTouchScreendevice() {
-    return 'ontouchstart' in window || navigator.maxTouchPoints;      
-};
+  return "ontouchstart" in window || navigator.maxTouchPoints;
+}
